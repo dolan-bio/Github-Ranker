e578494180567320d9d40a5e964ac93170695a14
Dolan <dolan_miu@hotmail.com>
2017-07-25 03:11:17 +0100

    Made the model schema

--- src/counter/snapshot-model.ts
+++ src/counter/snapshot-model.ts
@@ -1,12 +1,26 @@
 import * as mongoose from "mongoose";
 
 interface ISnapshotDocument extends mongoose.Document {
-    lastUserId: number;
+    user: {
+        from: number,
+        to: number,
+        count: number,
+    };
+    average: number;
 }
 
 const SnapshotSchema = new mongoose.Schema({
-    lastUserId: Number,
+    user: {
+        from: Number,
+        to: Number,
+    },
+    average: Number,
 });
+
+SnapshotSchema.virtual("user.count")
+    .get(function(): number {
+        return Math.abs(this.user.to - this.user.from);
+    });
 
 const Snapshot = mongoose.model<ISnapshotDocument>("GithubRankerSnapshot", SnapshotSchema);
 
