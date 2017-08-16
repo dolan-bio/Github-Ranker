4bb340464c155c9c569a45a489fb16cfde634102
Dolan <dolan_miu@hotmail.com>
2017-08-16 20:49:25 +0100

    Fix linting

--- src/counter/index.ts
+++ src/counter/index.ts
@@ -1,5 +1,5 @@
 import * as _ from "lodash";
-import { WriteOpResult } from "mongodb";
+// import { WriteOpResult } from "mongodb";
 import { Observable } from "rxjs/Rx";
 
 import { ContributionsFetcher } from "./contributions-fetcher";
